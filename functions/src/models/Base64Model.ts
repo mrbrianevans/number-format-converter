25ddd4d2067164e81f7431e0cffeed7b6c188d27
James Sheppard <63818299+jgsheppa@users.noreply.github.com>
2020-12-04 23:56:10 +0100

    Write function for base 10 to base 64 conversions

--- functions/src/models/Base64Model.ts
+++ functions/src/models/Base64Model.ts
@@ -1,15 +1,35 @@
-import {IModel} from "./IModel";
+import { IModel } from './IModel';
 
 // convert between base64 and decimal
 const Base64Model: IModel = {
-    encode: (decimal: number) => {
+  encode: (decimal: number) => {
+    var order =
+      '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-';
+    var base = order.length;
+    var str = '',
+      r;
+    while (decimal) {
+      r = decimal % base;
+      decimal -= r;
+      decimal /= base;
+      str = order.charAt(r) + str;
+    }
+    return str;
+  },
+  decode: (representation: string) => {
+    const order =
+      '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-';
+    const base = order.length;
+    let num = 0,
+      r;
+    while (representation.length) {
+      r = order.indexOf(representation.charAt(0));
+      representation = representation.substr(1);
+      num *= base;
+      num += r;
+    }
+    return num;
+  },
+};
 
-        return "Mw=="
-    },
-    decode: (representation: string) => {
-
-        return 0
-    },
-}
-
-export default Base64Model
+export default Base64Model;
