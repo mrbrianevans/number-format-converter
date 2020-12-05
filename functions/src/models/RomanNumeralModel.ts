00507d175e78e0496bc21583660e52c553a1d4c7
Daniel Mamphekgo <Danielromeo99@gmail.com>
2020-12-05 23:16:55 +0200

    Fixed :bug:Bug, of encode function of the RomanNumeral model

--- functions/src/models/RomanNumeralModel.ts
+++ functions/src/models/RomanNumeralModel.ts
@@ -3,59 +3,55 @@ import { IModel } from './IModel'
 // convert between roman numerals and decimal
 const RomanNumeralModel: IModel = {
     encode: (decimal: number) => {
-        const arrayOfLetters = []
-        let num: number = decimal
-        let largestBase: number
-        let quotient: number
-        let remainder: number
+        let arrayOfLetters = [];
+		let num: number = decimal;
+		let largestBase: number ;
+		let quotient: number;
+		let remainder: number;
+		
+		const arr = [
+			 {I : 1},{IV: 4},{V : 5},{VI : 9},{X : 10},{XL : 40},{L : 50},{XC : 90},{C : 100},{CD : 400},{D : 500},{CM : 900},{M : 1000}
+		];
 
-        const arr = [
-            { I: 1 },
-            { IV: 4 },
-            { V: 5 },
-            { VI: 9 },
-            { X: 10 },
-            { XL: 40 },
-            { L: 50 },
-            { XC: 90 },
-            { C: 100 },
-            { CD: 400 },
-            { D: 500 },
-            { CM: 900 },
-            { M: 1000 }
-        ]
+		const getKey = function(value: number){ 
+     
+			for (let j = 0; j < arr.length; j++) {
+				if(Object.values(arr[j])[0] == value){
+					return Object.keys(arr[j])[0];
+				}
+				continue;
+			}
+			return;
+		}
+		
+		for (let i = 0; i < arr.length; i++) {
+			let objectsValue ;
+			objectsValue = Object.values(arr[i])[0]; 
+			// if the num is exaclty a letter, then return that letter:	
+			if(objectsValue != undefined &&  (objectsValue  /  decimal) == 1 ){
+				return Object.keys(arr[i])[0];
+			}else{
+				continue;
+			}
+		}
 
-        const getKey = function(value: number) {
-            for (const item of arr) {
-                if (Object.values(item)[0] === value) {
-                    return Object.keys(item)[0]
-                }
-                return
-            }
-            return
-        }
+		for (let i = arr.length - 1; i >= 0; i--) {
+			let objectsValue ;
+			objectsValue = Object.values(arr[i])[0]; 
 
-        for (let i = 0; i < arr.length; i++) {
-            const objectsValue = Object.values(arr[i])[0]
-            // if the num is exaclty a letter, then return that letter:
-            if (objectsValue !== undefined && objectsValue / decimal === 1) {
-                return Object.keys(arr[i])[0]
-            }
-        }
 
-        for (let i = arr.length - 1; i >= 0; i--) {
-            const objectsValue = Object.values(arr[i])[0]
-            if (objectsValue !== undefined && objectsValue <= num) {
-                largestBase = objectsValue
-                quotient = num / largestBase
-                remainder = num - quotient * largestBase
-                for (let k = 0; k < quotient; k++) {
-                    arrayOfLetters.push(getKey(largestBase))
-                }
-                num = remainder
-            }
-        }
-        return arrayOfLetters.join('')
+			if(objectsValue != undefined && (objectsValue) <= num ){
+				largestBase = objectsValue;  
+				quotient = num /largestBase;
+        quotient = Math.floor(quotient)
+				remainder = num - (quotient *largestBase);
+				for (let k = 0; k < quotient; k++) {
+          arrayOfLetters.push( getKey(largestBase));
+				}
+				num = remainder;
+			}			
+		}		
+		return arrayOfLetters.join('');
     },
 
     decode: (representation: string) => {
