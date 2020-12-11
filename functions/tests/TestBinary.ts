8253e48172a3219fe928cee9ab2f75796dc32d17
Brian Evans <ebrian101@gmail.com>
2020-12-11 22:55:48 +0000

    Fixed binary tests format error
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- functions/tests/TestBinary.ts
+++ functions/tests/TestBinary.ts
@@ -11,11 +11,11 @@ describe('binary', () => {
         expect(BinaryModel.encode(357)).to.equal('101100101')
     })
     it('decode', () => {
-        expect(BinaryModel.decode('1001100101100')).to.equal(300)
-        expect(BinaryModel.decode('1001100111010')).equal(314)
-        expect(BinaryModel.decode('100110011111')).equal(159)
-        expect(BinaryModel.decode('1000100001001')).equal(265)
-        expect(BinaryModel.decode('1011101100101')).equal(357)
+        expect(BinaryModel.decode('100101100')).to.equal(300)
+        expect(BinaryModel.decode('100111010')).equal(314)
+        expect(BinaryModel.decode('10011111')).equal(159)
+        expect(BinaryModel.decode('100001001')).equal(265)
+        expect(BinaryModel.decode('101100101')).equal(357)
     })
 
     it('convert', () => {
