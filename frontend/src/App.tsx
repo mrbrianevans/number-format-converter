1e9b06f7895d1f166208f97f7173ecca3cc504ec
castynet <riungemaina@gmail.com>
2020-12-16 17:47:31 +0300

    frontend/src/App: styled & fine-tuned

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import './css/app.css'
 import Footer from './components/footer'
 import Contributors from './components/contributors'
@@ -7,11 +8,27 @@ import Converter from './components/converter'
 const App: React.FC = () => {
   return (
     <div className='app'>
-      <Converter />
-      <Contributors />
+      <FillPage>
+        <PageHeader>Number Format Converter</PageHeader>
+        <hr />
+        <Converter />
+        <Contributors />
+      </FillPage>
       <Footer />
     </div>
   )
 }
+
+const FillPage = styled.div`
+  /* make site content fill the page - in future & with the addition of more
+  stuff to fill out the page naturally, this can be deleted */
+  min-height: 90vh;
+`
+
+const PageHeader = styled.h1`
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+`
 
 export default App
