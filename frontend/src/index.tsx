75ff5d4dc967bdcf65a1777d07a7f6067bc7b940
castynet <riungemaina@gmail.com>
2020-12-16 17:46:57 +0300

    frontend/src/index: removed bootstarp

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,12 +4,6 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-// import bootstrap
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
-// end of bootstrap imports
-
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
