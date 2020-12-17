0342ac6ceb565ab187003a2ed2b531bc3d9d2a81
castynet <riungemaina@gmail.com>
2020-12-17 16:45:23 +0300

    frontend/src/components/footer: some basic restructuring & styled the footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,31 +1,52 @@
 import React from 'react'
-import '../css/footer.css'
+import styled from 'styled-components'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer">
-      <div className="container text-center">
-        <div className="row">
-          <div className="col-md-6">
-            <a
-              className="App-link"
-              href="https://github.com/mrbrianevans/number-format-converter"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img alt="link to github repository" src="github-logo.png" />
-            </a>
-          </div>
+    <FooterWrap>
+      <ProjectLink>
+        See the Project on{'  '}
+        <a
+          className='App-link'
+          href='https://github.com/mrbrianevans/number-format-converter'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Github <Image alt='link to github repository' src='github-logo.png' />
+        </a>
+      </ProjectLink>
 
-          <div className="col-md-6 developed-by">
-            Maintained by{' '}
-            <a href="http://brianevans.tech/">Brian Evans</a> and{' '}
-            <a href="https://macbase.co.za/">Daniel Mamphekgo</a>
-          </div>
-        </div>
-      </div>
-    </footer>
+      <Maintainers>
+        Maintained by{' '}
+        <a target='_blank' href='http://brianevans.tech/'>
+          Brian Evans
+        </a>{' '}
+        and{' '}
+        <a target='_blank' href='https://macbase.co.za/'>
+          Daniel Mamphekgo
+        </a>
+      </Maintainers>
+    </FooterWrap>
   )
 }
+
+const Image = styled.img`
+  height: 15px;
+`
+
+const FooterWrap = styled.footer`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 30px;
+  background-color: rgb(0 0 0 / 0.1);
+  border-top: 1px solid;
+  bottom: 0;
+  padding: 20px 10px;
+`
+
+const ProjectLink = styled.div``
+
+const Maintainers = styled.div``
 
 export default Footer
